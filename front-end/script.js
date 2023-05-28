268468504104896409e9e324679c8344b52d507c
fugzel <aleksandar.d.denev@gmail.com>
2023-05-28 14:55:28 +0300

    front changes

--- front-end/script.js
+++ front-end/script.js
@@ -1,17 +1,18 @@
-// Templates
+                    // Templates
 tPolygon = () => document.createElementNS("http://www.w3.org/2000/svg", "polygon");
 tSvg = () => document.createElementNS("http://www.w3.org/2000/svg", "svg");
+tText = () => document.createElementNS('http://www.w3.org/2000/svg', 'text');
 
-// Fetch
-const data = {
+                    // Fetch
+const data = { // Бих искал полигона на седалките всъщност да е няколко полигона, за да можем да четаем сложни седалки.
     "seats": [
     {
         "class": "First Class",
         "polygon": [
-        {"x": 0, "y": 0},
-        {"x": 50, "y": 0},
-        {"x": 50, "y": 50},
-        {"x": 0, "y": 50}
+            {"x": 0, "y": 0},
+            {"x": 50, "y": 0},
+            {"x": 50, "y": 50},
+            {"x": 0, "y": 50}
         ],
         "occurrences": [
         {"x": 15, "y": 25, "row": "A", "column": 1},
@@ -66,70 +67,116 @@ const data = {
 /*
 fetch('http://localhost:8080/api/your-endpoint')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => JSON.parse(data))
     .catch(error => console.error('Error:', error));
 */
 
-// Render
+                    // On click
+// My idea is to make an array of all popups to function as a queue composed of the popups. This way removing all other popups will be easy.
+function seatClick(seat, seatCenter) {
+    let seatPopup = document.createElement('div');
+    seatPopup.className = 'seatPopup';
+    
+    let titleText = document.createElement('h2');
+    titleText.textContent = `Seat: ${seat['column']} ${seat['row']}`;
+
+    let closeButton = document.createElement('button');
+    closeButton.textContent = 'Close';
+    closeButton.setAttribute('class', `clickable`);
+    closeButton.addEventListener('click', () => {
+        seatPopup.style.opacity = '0';
+        setTimeout(() => {
+            seatPopup.remove();
+        }, getElementDelay(seatPopup));
+    });
+    
+    seatPopup.appendChild(titleText);
+    seatPopup.appendChild(closeButton);
+    planeDiv.appendChild(seatPopup);
+
+    seatPopup.style.left = (seatCenter[0] - seatPopup.offsetWidth / 2) + 'px';
+    seatPopup.style.top = (seat['y'] + ((seatCenter[1] - seat['y']) * 2)) + 'px';
+
+    setTimeout(() => {seatPopup.style.opacity = '1';}, 0);
+    let popups = Array.from(document.getElementsByClassName('seatPopup'));
+    if (popups.length > 1) {
+        for (let i = 0; i < popups.length - 1; i++) {
+            popups[i].style.opacity = '0';
+    
+            setTimeout(() => {
+                if (document.body.contains(popups[i])) {
+                    popups[i].remove();
+                }
+            }, getElementDelay(popups[i]));
+        }   
+    }
+}
+
+function getElementDelay(element) {
+    let delay;
+    let rawTransDur = window.getComputedStyle(element).getPropertyValue('transition-duration');
+    if (rawTransDur.trim().split(/s|ms/)[1] === 'ms') {delay = parseInt(rawTransDur);} else {delay = parseFloat(rawTransDur) *1000;}
+    return delay;
+}
+
+                    // Render
 var planeDiv = document.getElementById('planeDiv');
-//planeDiv.innerHTML = data['plane']['svg'];
+var planeSvg = tSvg();
+planeSvg.setAttribute('id','planeSvg');
+planeSvg.setAttribute("width",'0px');
+planeSvg.setAttribute("height",'0px');
 
 data['seats'].forEach(clas => {
-    switch (clas['class']) {
-        default: {
-            var htmlSeatClass = 'error';
-        }
-        case 'Economy Class': {
-            var htmlSeatClass = 1;
-            break;
-        }
-        case 'Premium Economy Class':{
-            var htmlSeatClass = 4;
-            break;
-        }    
-        case 'Business Class':{
-            var htmlSeatClass = 2;
-            break;
-        }
-        case 'First Class':{
-            var htmlSeatClass = 3;
-            break;
-        }
-    }
+
+    let htmlSeatClass = [
+        'Economy Class','Business Class','First Class','Premium Economy Class'
+    ].indexOf(clas['class']) +1;
+
     clas['occurrences'].forEach(seat => {
-        var polygon = tPolygon();
-        polygon.setAttribute("class", `class${htmlSeatClass}`);
-        var svg = tSvg();
-        svg.setAttribute("width", 0);
-        svg.setAttribute("height", 0);
-        svg.setAttribute("class", 'svg');
-        clas['polygon'].forEach(coords => {
-            var point = svg.createSVGPoint();
-            point.x = coords['x'];
-            point.y = coords['y'];
-            polygon.points.appendItem(point);
-            polygon.class = 'class' + htmlSeatClass;
-            if (coords['x'] > svg.getAttribute("width")) {
-                svg.setAttribute("width", coords['x']);
+        let poly = tPolygon();
+        let pointsForCentroid = [];
+        clas['polygon'].forEach(geo => {
+            let point = planeSvg.createSVGPoint();
+            point.x = geo['x'] + seat['x'];
+            point.y = geo['y'] + seat['y'];
+            poly.points.appendItem(point);
+            
+            pointsForCentroid.push([point.x, point.y]);
+
+            if (point.x > parseInt(planeSvg.getAttribute("width"))) {
+                planeSvg.setAttribute("width", point.x + 'px');
             }
-            if (coords['y'] > svg.getAttribute("height")) {
-                svg.setAttribute("height", coords['y']);
+            if (point.y > parseInt(planeSvg.getAttribute("height"))) {
+                planeSvg.setAttribute("height", point.y + 'px');
             }
         })
-        planeDiv.style.width = 0;
-        planeDiv.style.height = 0;
-        console.log(seat['x'] + parseInt(svg.getAttribute("width")));
-        if (seat['x'] + parseInt(svg.getAttribute("width")) > parseInt(planeDiv.style.width)) {
-            planeDiv.style.width = (seat['x'] + parseInt(svg.getAttribute("width"))) + "px";
-        }
-        if (seat['y'] + parseInt(svg.getAttribute("height")) > parseInt(planeDiv.style.height)) {
-            planeDiv.style.height = (seat['y'] + parseInt(svg.getAttribute("height"))) + "px";
-        }
-        console.log(parseInt(planeDiv.style.width));
-        svg.setAttribute("style", `top:${seat['y']}px; left:${seat['x']}px`);
-        svg.appendChild(polygon);
-        planeDiv.appendChild(svg);
-        
+        let polyCenter = d3.polygonCentroid(pointsForCentroid);
 
-    })
-})
+        poly.setAttribute('class', `class${htmlSeatClass} clickable`);
+        poly.addEventListener('click', () => seatClick(seat, polyCenter));
+        poly.addEventListener('mouseenter', () => poly.classList.add(`class${htmlSeatClass}_hover`));
+        poly.addEventListener('mouseleave', () => poly.classList.remove(`class${htmlSeatClass}_hover`));
+ 
+        planeSvg.appendChild(poly);
+
+        let text = tText();
+        text.textContent = `${seat['column']} ${seat['row']}`;
+        text.setAttribute('class', `clickable`);
+        text.setAttribute('dominant-baseline', 'middle');
+        text.addEventListener('click', () => seatClick(seat, polyCenter));
+        text.addEventListener('mouseenter', () => poly.classList.add(`class${htmlSeatClass}_hover`));
+        text.addEventListener('mouseleave', () => poly.classList.remove(`class${htmlSeatClass}_hover`));
+        
+        planeSvg.appendChild(text);
+        
+        setTimeout(() => {
+            let bbox = text.getBBox();
+            text.setAttribute('x', polyCenter[0] - (bbox.width / 2));
+            text.setAttribute('y', polyCenter[1]);
+        }, 0);
+    });
+});
+
+
+
+planeDiv.appendChild(planeSvg);
