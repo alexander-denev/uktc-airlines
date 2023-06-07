adbbeb868177742154b079da09ee3cd1420ba022
fugzel <aleksandar.d.denev@gmail.com>
2023-06-07 20:22:37 +0300

    .

--- seat generator/script.js
+++ seat generator/script.js
@@ -1,19 +1,113 @@
-fetch('input.txt')
+const classDef = {
+  "Economy Class": [14, 17, 18, 19]
+};
+
+let planeModel = "Airbus A320"
+let floor = 1;
+let rotation = 0;
+
+fetch('inputSeats.svg')
   .then(response => response.text())
   .then(data => {
 
+    // Parse the airplane svg
     let parser = new DOMParser();
     let theObj = parser.parseFromString(data, "image/svg+xml").documentElement;
 
+    // Append svg to DOM for processing
     document.body.appendChild(theObj);
 
+    // Get the coordinates of the seats from the svg
     let output = [];
     Array.from(theObj.children.seats.children).forEach(seat => {
-        seat = seat.getBBox();
-        output.push({"x":seat.x, "y":seat.y});
+        let seatBB = seat.getBBox();
+
+        let clas = "default";
+        for (let [key, value] of Object.entries(classDef)) {
+          if (value.includes(parseInt(seat.children[0].classList[0].match(/\d+/)[0]))) {
+            clas = key;
+          } else {
+            clas = "doesn't include"
+          }
+        }
+        
+        output.push({
+          "x": Math.floor(seatBB.x), 
+          "y": Math.floor(seatBB.y), 
+          "seatClass": clas,
+          "floor": floor,
+          "rotation": rotation
+        });
+    });
+  
+    // Sort
+    output.sort(function(a, b) {
+      if (a.y === b.y) {
+        return a.x - b.x;
+      } else {
+        return a.y - b.y;
+      }
     });
     
+    // Fix tiny differences in coordinates
+    let previous;
+    output.forEach(seat => {
+      if (!previous) {
+        previous = seat;
+        return;
+      }
+      
+      if (previous.y - seat.y < 3 && previous.y - seat.y > -3) {
+        output[output.indexOf(seat)].y = previous.y; 
+      }
+      previous = seat;
+    });
+
+    // Calculate seat names
+    let rows = [];
+    let columnCounter = 0;
+    output.forEach(seat => {
+      if (!rows.includes(seat.y)){
+        rows.push(seat.y);
+        columnCounter = 0;
+      }
+
+      output[output.indexOf(seat)].row = rows.indexOf(seat.y) + 1;
+      output[output.indexOf(seat)].column = String.fromCharCode(65 + columnCounter);
+      columnCounter += 1;
+    });
+
     console.log(output);
 
+    fetch('inputSeats.svg')
+    .then(response => response.text())
+    .then(planeData => {
+
+      let button = document.createElement("button");
+      button.textContent = "POST";
+
+      button.addEventListener("click", () => thePost(button, output, planeData, planeModel));
+      document.body.innerHTML = "";
+      document.body.appendChild(button);
+    });
 });
+
+async function thePost(self, seats, planeSvg, planeModel){
+  
+  self.remove();
+
+  fetch("output.txt", {
+    method: "POST",
+    body: JSON.stringify({
+      "seats": seats,
+      "planeVisualisation": planeSvg,
+      "planeName": planeModel
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then(response => response.json())
+    .then(json => console.log(json));
+}
 
